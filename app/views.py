0b6f5532e7169928cc64bcbafecc24ad7cedd6f2
devseme <ian.ochenge@student.moringaschool.com>
2021-10-31 15:30:18 +0300

    created control flow templates

--- app/views.py
+++ app/views.py
@@ -9,8 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/source/<int:source_id>')
 def source(source_id):
